c6b1237c8848e17bd797dcf4611e1ce4383bff64
wanawsha <wanawshaak@gmail.com>
2026-05-09 21:15:45 +0200

    fetch and display venues from API

---
+++ src/api/venues.js
@@ -0,0 +1,18 @@
+const BASE_URL = "https://v2.api.noroff.dev/holidaze/venues";
+
+export async function getVenues() {
+    try {
+        const response = await fetch(BASE_URL);
+
+        if (!response.ok) {
+        throw new Error("Failed to fetch venues");
+        }
+
+        const data = await response.json();
+
+        return data.data;
+    } catch (error) {
+        console.error(error);
+        return [];
+    }
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,35 @@
+import { useEffect, useState } from "react";
+import { getVenues } from "../api/venues";
+
 function HomePage() {
-  return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  );
+
+    const [venues, setVenues] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+        const data = await getVenues();
+        setVenues(data);
+        }
+
+        fetchData();
+    }, []);
+
+
+    return (
+        <div>
+        <h1>Home</h1>
+        {venues.length === 0 && <p>Loading venues...</p>}
+        <div>
+            {venues.map((venue) => (
+            <div key={venue.id} style={{ marginBottom: "20px" }}>
+                <h2>{venue.name}</h2>
+                <p>{venue.location?.city}</p>
+                <p>${venue.price} per night</p>
+            </div>
+            ))}
+        </div>
+        </div>
+    );
 }
 
 export default HomePage;
